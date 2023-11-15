636ffc7c57f8c592d10190b7b0d16880332fd3ed
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2023-11-15 10:27:23 +0200

    feat: setup sign-in with google

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,41 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+services.AddControllers();
+
+services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Auth:client_id"];
+        options.ClientSecret = builder.Configuration["Auth:client_secret"];
+    });
+
+services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.HttpOnly = HttpOnlyPolicy.None;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCookiePolicy();
+
+app.UseAuthentication();
+
+app.MapControllers();
 
 app.Run();
---
+++ backend/Src/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Src.Controllers;
+[Route("/api/v1/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpGet("login")]
+    public IActionResult Login() 
+    {
+        var redirectUrl = Url.Action("externalLoginCallback", "Auth", null, Request.Scheme);
+        var properties = new AuthenticationProperties() { RedirectUri = redirectUrl };
+        return Challenge(properties, "Google");
+    }
+    [HttpGet("externalLoginCallback")]
+    public async Task<IActionResult> ExternalLoginCallback() 
+    {
+        var info = await HttpContext.AuthenticateAsync("Google");
+        if (info.Succeeded)
+        {
+            return Ok(new { Message = "Authentication Succesfull" });
+        }
+        return Unauthorized();
+    }
+}
